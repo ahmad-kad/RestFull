2ee4f4a03ea721dd977e2bbe49413d1f8e320554
Ahmad Kaddoura <kaddoura.ahmad@gmail.com>
2024-01-15 13:07:16 -0800

    Initial Commit

---
+++ RestFull/RestFullApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestFullApp.swift
+//  RestFull
+//
+//  Created by ahmad kaddoura on 1/15/24.
+//
+
+import SwiftUI
+
+@main
+struct RestFullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
